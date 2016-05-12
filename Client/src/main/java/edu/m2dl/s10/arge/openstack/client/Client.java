6a605cb730ed1839cde4809748e73d48f3e98d8b
manantsoa <manantsoa.razana@gmail.com>
2016-05-12 08:53:51 +0200

    First COmmit

---
+++ Client/src/main/java/edu/m2dl/s10/arge/openstack/client/Client.java
@@ -0,0 +1,108 @@
+package edu.m2dl.s10.arge.openstack.client;
+
+import org.apache.xmlrpc.client.XmlRpcClient;
+import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
+import org.apache.xmlrpc.client.XmlRpcCommonsTransportFactory;
+import org.apache.xmlrpc.webserver.ServletWebServer;
+
+import java.net.URL;
+
+import static java.lang.Thread.sleep;
+
+/**
+ * Created by cedricrohaut on 25/03/2016.
+ */
+public class Client {
+    private static int nbReq;
+    private static String ipRepartiteur;
+    private static String port;
+
+    public Client(int nbReq, String ipRepartiteur, String port) {
+        this.nbReq = nbReq;
+        this.ipRepartiteur = ipRepartiteur;
+        this.port = port;
+    }
+
+    public int getNbReq() {
+        return nbReq;
+    }
+
+    public void setNbReq(int nbReq) {
+        this.nbReq = nbReq;
+    }
+
+    public String getIpRepartiteur() {
+        return ipRepartiteur;
+    }
+
+    public void setIpRepartiteur(String ipRepartiteur) {
+        this.ipRepartiteur = ipRepartiteur;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    public static void main(String args[]) throws Exception {
+
+        if (args.length != 3) {
+            throw new ServletWebServer.Exception(1, "\nErreur dans les arguments : nombreRequetes ipRepartiteur port", "nombreRequetes ipRepartiteur port");
+        }
+        String nbReqTemp= args[0];
+        nbReq = Integer.parseInt(nbReqTemp);
+        ipRepartiteur = args[1];
+        port = args[2];
+
+        System.out.println("Le nom du thread principal est " + Thread.currentThread().getName());
+
+        ThreadUpdate t = new ThreadUpdate("ThreadUpdate");
+        t.start();
+
+        String url = ipRepartiteur + ":" + port + "/request";
+        System.out.println(url);
+
+        // create configuration
+        XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
+        config.setServerURL(new URL(url));
+        config.setEnabledForExtensions(true);
+        config.setConnectionTimeout(60 * 1000);
+        config.setReplyTimeout(60 * 1000);
+
+        XmlRpcClient client = new XmlRpcClient();
+
+        // use Commons HttpClient as transport
+        client.setTransportFactory(
+                new XmlRpcCommonsTransportFactory(client));
+        // set configuration
+        client.setConfig(config);
+        t.setNbReq(nbReq);
+
+        while (true) {
+            nbReq = t.getNbReq();
+            //System.out.println("Main - nombre de requêtes " + nbReq);
+
+            for (int i = 0 ; i < nbReq ; i++) {
+                // make the a regular call
+                Object[] params = new Object[]
+                        { new Integer(2), new Integer(3) };
+               // Integer result = (Integer) client.execute("Repartiteur.request", params);
+               // System.out.println("2 + 3 = " + result);
+                //System.out.println("" + i);
+
+            }
+            sleep(1000);
+        }
+
+
+        // make a call using dynamic proxy
+	  /*          ClientFactory factory = new ClientFactory(client);
+          Adder adder = (Adder) factory.newInstance(Adder.class);
+          int sum = adder.add(2, 4);
+          System.out.println("2 + 4 = " + sum);
+	  */
+    }
+}
---
+++ Repartiteur/src/main/java/edu/m2dl/s10/arge/openstack/repartiteur/Repartiteur.java
@@ -0,0 +1,123 @@
+package edu.m2dl.s10.arge.openstack.repartiteur;
+
+import org.apache.xmlrpc.XmlRpcException;
+import org.apache.xmlrpc.server.PropertyHandlerMapping;
+import org.apache.xmlrpc.server.XmlRpcServer;
+import org.apache.xmlrpc.server.XmlRpcServerConfigImpl;
+import org.apache.xmlrpc.webserver.WebServer;
+import org.openstack4j.api.OSClient;
+import org.openstack4j.openstack.OSFactory;
+
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * Created by julien on 25/03/16.
+ */
+public class Repartiteur implements Runnable {
+
+    private String port;
+
+    public static OSClient os;
+
+    public static void main(String[] args) {
+        if(args.length != 1) {
+            System.out.println("Il faut fournir le numéro du port");
+        }
+
+        String port = args[0];
+
+        // Boucle de reception des requetes
+
+        Repartiteur r = new Repartiteur(port);
+        r.run();
+
+    }
+
+    public void run() {
+
+        os = connectToCloud();
+        WebServer webServer = new WebServer(Integer.parseInt(port));
+
+        XmlRpcServer xmlRpcServer = webServer.getXmlRpcServer();
+
+        PropertyHandlerMapping phm = new PropertyHandlerMapping();
+          /* Load handler definitions from a property file.
+           * The property file might look like:
+           *   Calculator=org.apache.xmlrpc.demo.Calculator
+           *   org.apache.xmlrpc.demo.proxy.Adder=org.apache.xmlrpc.demo.proxy.AdderImpl
+           */
+        try {
+            phm.addHandler("Calculateur", edu.m2dl.s10.arge.openstack.calculateur.Calculateur.class);
+            System.out.println("Lancement du serveur sur le port " + port);
+        } catch (XmlRpcException e) {
+            e.printStackTrace();
+        }
+
+          /* You may also provide the handler classes directly,
+           * like this:
+           * phm.addHandler("Calculator",
+           *     org.apache.xmlrpc.demo.Calculator.class);
+           * phm.addHandler(org.apache.xmlrpc.demo.proxy.Adder.class.getName(),
+           *     org.apache.xmlrpc.demo.proxy.AdderImpl.class);
+           */
+        xmlRpcServer.setHandlerMapping(phm);
+
+        XmlRpcServerConfigImpl serverConfig =
+                (XmlRpcServerConfigImpl) xmlRpcServer.getConfig();
+        serverConfig.setEnabledForExtensions(true);
+        serverConfig.setContentLengthOptional(false);
+
+        try {
+            webServer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        while(true) {
+
+
+
+            // Serveur : Récupérer les requetes de l'updateRepartiteur
+
+
+            // Client : Contacter les Calculateur
+
+        }
+
+    }
+
+    public Repartiteur(String port) {
+        this.port = port;
+    }
+
+    public void add(String ip, String port) {
+        System.out.println("AJOUTE UN CALCULATEUR ["+ip+":"+port+"]");
+
+    }
+
+    public void del(String ip, String port) {
+        // Appel de Léo
+        System.out.println("SUPPRIME UN CALCULTEUR ["+ip+":"+port+"]");
+
+    }
+
+    public void request() {
+        // Appel du client
+        System.out.println("REDIRIGE LA REQUETE VERS UN CALCULATEUR");
+    }
+
+    public static OSClient connectToCloud() {
+        System.out.print("Connection");
+        OSClient os = OSFactory.builder()
+                .endpoint("http://195.0.0.1:5000/v2.0")
+                .credentials("ens18", "LEBWJ1")
+                .tenantName("service").authenticate();
+
+
+        System.out.println("Success");
+        System.out.println();
+        return os;
+    }
+}
